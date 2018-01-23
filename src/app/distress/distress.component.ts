49bfee65c5fd79a92d1ddd90607fdb812d0b57f9
ashinzekene <ashinzekene@gmail.com>
2018-01-23 15:59:26 +0100

    added many things

--- src/app/distress/distress.component.ts
+++ src/app/distress/distress.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { Distress } from '../models';
 
@@ -9,12 +10,19 @@ import { Distress } from '../models';
 })
 export class DistressComponent implements OnInit {
   distress: Distress
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private meta: Meta) { }
 
   ngOnInit() {
-    this.route.data.subscribe((data: {distress: Distress}) => {
+    this.route.data.subscribe((data: { distress: Distress }) => {
       console.log("Distress", data)
       this.distress = data.distress
+      this.meta.addTags([
+        { name: 'og:url', content: window.location.href },
+        { name: 'og:type', content: "article" },
+        { name: 'og:title', content: data.distress.title },
+        { name: 'og:description', content: data.distress.description.substring(0, 51) },
+        { name: 'og:keywords', content: data.distress.tags && data.distress.tags.join(",") }
+      ])
     })
   }
 
