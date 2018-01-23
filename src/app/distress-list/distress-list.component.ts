4601646cd118f9def3085dffe6da99ff5388c333
ashinzekene <ashinzekene@gmail.com>
2018-01-23 15:58:41 +0100

    re-designed

--- src/app/distress-list/distress-list.component.ts
+++ src/app/distress-list/distress-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DistressService } from '../core';
 import { Distress } from '../models/index';
+import { distanceInWords } from "date-fns";
 
 @Component({
   selector: 'app-distress-list',
@@ -8,12 +9,17 @@ import { Distress } from '../models/index';
   styleUrls: ['./distress-list.component.css']
 })
 export class DistressListComponent implements OnInit {
+  today = Date.now()
   distresses: Distress[]
   searchParams = {
     limit: 20,
     orderBy: "createdAt"
   }
   constructor(private distressService: DistressService) { }
+
+  distanceInWords(date, suffix?: boolean) {
+    return distanceInWords(Date.now(), date, { addSuffix: suffix })
+  }
 
   ngOnInit() {
     this.distressService.search(this.searchParams)
