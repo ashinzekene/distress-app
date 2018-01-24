98f9c163c15404a09dc8c771828088afa96bb10d
ashinzekene <ashinzekene@gmail.com>
2018-01-24 20:08:13 +0100

    fixed routing issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,15 @@ import { CreateComponent } from './create/create.component';
 import { DistressComponent } from './distress/distress.component';
 import { DistressResolver } from './distress-resolver.service';
 import { DistressListComponent } from './distress-list/distress-list.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent  },
   { path: 'create', component: CreateComponent  },
   { path: 'distresses', component: DistressListComponent  },
   { path: 'distress/:id', component: DistressComponent, resolve: { distress: DistressResolver } },
-  { path: '', component: HomeComponent, pathMatch: "full" },
+  { path: '', redirectTo: '/home', pathMatch: "full" },
+  { path: '*', component: NotFoundComponent },
 ];
 
 @NgModule({
