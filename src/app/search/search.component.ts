3550f2bdab9a57113bfeb23a7447386684747567
ashinzekene <ashinzekene@gmail.com>
2018-01-25 17:49:32 +0100

    search now works

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Categories } from '../models/index';
+import { Categories, Distress } from '../models';
+import { DistressService } from '../core';
+import { distanceInWordsStrict } from "date-fns";
 
 @Component({
   selector: 'app-search',
@@ -7,9 +9,51 @@ import { Categories } from '../models/index';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  categories = Categories
-  constructor() { }
+  public distresses: Distress[]
+  public categories = Categories
+  public orderOptions = [
+    { name: 'Date', value: 'date', ascending: true },
+    { name: 'Approvals', value: 'approves', ascending: true },
+    { name: 'Comments', value: 'comments', ascending: true },
+  ]
+  public params = {
+    title: '',
+    orderBy: '',
+    ascending: true,
+    categories: []
+  }
+  public noDistress: boolean= false
+  public searching: boolean= false
+  public hasReturnedResult: boolean= false
+  public selectedOrder: string = this.orderOptions[0].name
+  public selectedCategories = {}
+  constructor(private distressService: DistressService) { }
 
+  search() {
+    if (this.params.title.length < 3) {
+      return 
+    }
+    this.searching = true
+    this.noDistress = false
+    let params = Object.assign({}, this.params, { categories: Object.keys(this.selectedCategories) })
+    this.distressService.search(params).subscribe((distresses: Distress[]) => {
+      this.searching = false
+      this.noDistress = !distresses.length
+      this.distresses = distresses
+      console.log(`fetched ${distresses}`)
+    })
+    console.log(params)
+  }
+  
+  changeOrder(x: number) {
+    this.selectedOrder = this.orderOptions[x].name
+    this.params.orderBy = this.orderOptions[x].value
+  }
+
+  distanceInWords(date, suffix?: boolean) {
+    return distanceInWordsStrict(Date.now(), date, { addSuffix: suffix })
+  }
+  
   ngOnInit() {
   }
 
