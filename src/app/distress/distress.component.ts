d55915e8222de1e29ce0dc878f060fcf30c192b5
ashinzekene <ashinzekene@gmail.com>
2018-01-27 07:33:19 +0100

    user can log in and comment, retrieve comments

--- src/app/distress/distress.component.ts
+++ src/app/distress/distress.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Meta } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { Distress } from '../models';
-import { SocialAuthService } from '../core';
+import { SocialAuthService, UserService, DistressService } from '../core';
+import { distanceInWordsToNow } from 'date-fns';
 
 @Component({
   selector: 'app-distress',
@@ -10,19 +11,54 @@ import { SocialAuthService } from '../core';
   styleUrls: ['./distress.component.css']
 })
 export class DistressComponent implements OnInit {
-  distress: Distress
-  constructor(private route: ActivatedRoute, private meta: Meta, private socialAuth: SocialAuthService) { }
+  public distress: Distress
+  public distressComments: any[]
+  public isAuth:boolean = false
+  public user:any
+  public textComment: string = ""
+  
+  constructor(
+    private route: ActivatedRoute,
+    private meta: Meta,
+    private distressService: DistressService,
+    private userService: UserService,
+    private socialAuth: SocialAuthService) { }
 
-  comment() {
+  signIn(provider) {
     this.socialAuth.getUser().subscribe(user => {
       console.log("authenticating...")
       if (!user) {
-        this.socialAuth.signInWithFB()
+        this.socialAuth.signIn(provider).then(user => {
+          this.userService.createUser(user).subscribe(res => {
+            console.log("logged user", res)
+          })
+        })
       } else {
-        console.log(user)
+       this.user = user
+       this.isAuth = !!user
       }
     })
   }
+
+  comment() {
+    if (this.textComment .length < 5) {
+      return
+    }
+    let comment = {
+      distress: this.distress._id,
+      email: this.user.email,
+      text: this.textComment
+    }
+    console.log("sending...", comment)
+    this.distressService.comment(comment)
+      .subscribe(newComment => {
+        console.log("Comment ", newComment)
+      })
+  }
+
+  distanceInWords(date) {
+    return distanceInWordsToNow(date)
+  } 
 
   ngOnInit() {
     this.route.data.subscribe((data: { distress: Distress }) => {
@@ -35,7 +71,15 @@ export class DistressComponent implements OnInit {
         { name: 'og:description', content: data.distress.description.substring(0, 51) },
         { name: 'og:keywords', content: data.distress.tags && data.distress.tags.join(",") }
       ])
+      this.distressService.getComments(data.distress._id)
+        .subscribe(comments => {
+          console.log('comments ', comments)
+          this.distressComments = comments
+        })
+    })
+    this.socialAuth.getUser().subscribe(user => {
+      this.isAuth = !!user
+      this.user= user
     })
   }
-
 }
