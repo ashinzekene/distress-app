bfeeeb69f5242b22307f9479462cbcb653d2ae20
ashinzekene <ashinzekene@gmail.com>
2018-02-02 06:50:25 +0100

    pagination added

--- src/app/distress-list/distress-list.component.ts
+++ src/app/distress-list/distress-list.component.ts
@@ -18,13 +18,41 @@ export class DistressListComponent implements OnInit {
     limit: 20,
     orderBy: "createdAt"
   }
+  currInd: number = 0
+  disressLength: number
+  pages: any[] = []
   constructor(private distressService: DistressService, private route: ActivatedRoute) { }
 
   distanceInWords(date, suffix?: boolean) {
     return distanceInWords(Date.now(), date, { addSuffix: suffix })
   }
 
+  getFor(i) {
+    if (i === 'next') {
+      i = this.currInd+1
+    }
+    if (i === 'prev') {
+      i = this.currInd+1
+    }
+    this.searchParams.offset = 20 * i
+    this.currInd = i
+    this.distressService.search(this.searchParams)
+      .subscribe(distresses => {
+        this.distresses = distresses.map(distress => {
+          distress.description.substr(0, 145)
+          return distress
+        })
+      })
+  }
+
   ngOnInit() {
+    this.distressService.distressLength().subscribe(res => {
+      this.disressLength = res.result
+      let pages = res.result / 20
+      for (let x = 0; x <= pages; x++) {
+        this.pages.push(x)
+      }
+    })
     this.route.queryParams.map(params => {
       params['limit'] ? this.searchParams.limit = params['limit'] : null
       params['offset'] ? this.searchParams.offset = params['offset'] : null
